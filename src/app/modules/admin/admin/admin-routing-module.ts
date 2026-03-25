7a053b94a9556455bab2ef974e1b3726f232f5a5
brayan <bspc1507@gmail.com>
2026-03-25 13:06:20 -0500

    fix: corrección de rutas

--- src/app/modules/admin/admin/admin-routing-module.ts
+++ src/app/modules/admin/admin/admin-routing-module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from '../modules/admin/pages/dashboard/dashboard';
+import { DashboardComponent } from '../modules/pages/dashboard/dashboard';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent }
--- src/app/modules/cliente/cliente/cliente-routing-module.ts
+++ src/app/modules/cliente/cliente/cliente-routing-module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../modules/cliente/pages/home/home';
+import { HomeComponent } from '../modules/pages/home/home';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }
