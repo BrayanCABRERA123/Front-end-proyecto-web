807ecd7bde8146ba37ee14943a2cdbefd0663c81
camiloguilombo12 <cguilombo587@gmail.com>
2026-03-26 08:51:19 -0500

    feat: agregue la logica y validaciones del formulario de registro

--- src/app/auth/auth/auth-routing-module.ts
+++ src/app/auth/auth/auth-routing-module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
-import { Register } from './pages/register/register';
+import { RegisterComponent } from './pages/register/register.component';
 import { ForgotPassword } from './pages/forgot-password/forgot-password';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: Register },
+  { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPassword },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
