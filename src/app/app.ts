9352b5232df04cc924f7232ce7aa18f5d62882d2
camiloguilombo12 <cguilombo587@gmail.com>
2026-04-22 14:16:06 -0500

    feat: Apply methods to the app.ts file to save the user's chosen languages ​​and themes on any screen and maintain them.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,20 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.html'
 })
-// Cambiamos el nombre a App para que coincida con main.ts
-export class App { }
+export class App {
+
+  constructor(private translate: TranslateService) {
+    this.initApp();
+  }
+
+  initApp() {
+    //idioma guardado, español va por defecto
+    const lang = localStorage.getItem('lang') || 'es';
+    this.translate.setDefaultLang('es');
+    this.translate.use(lang);
+
+    //tema guardado
+    const theme = localStorage.getItem('theme') || 'light';
+    document.body.className = theme;
+  }
+}
