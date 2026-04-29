9c094a156ba153568ad225a58f3aeb702779fc01
camiloguilombo12 <cguilombo587@gmail.com>
2026-04-29 12:50:03 -0500

    feat: Change of theme and language on the landing, dynamic logo and new features and colors in styles.scss.

--- src/app/features/loanding/pages/landing/landing.ts
+++ src/app/features/loanding/pages/landing/landing.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatIconModule, TranslateModule],
   templateUrl: './landing.html',
   styleUrl: './landing.scss'
 })
@@ -14,20 +15,20 @@ export class LandingComponent {
 
   // características que se muestran en la sección "¿Por qué elegirnos?"
   features = [
-    {
-      icono: 'water_drop',
-      titulo: 'Lavado Ecológic',
-      descripcion: 'Usamos productos biodegradable'
-    },
-    {
-      icono: 'shield',
-      titulo: 'Protección Total',
-      descripcion: 'Cuidamos cada detalle de tu auto'
-    },
-    {
-      icono: 'star',
-      titulo: '5 Estrellas',
-      descripcion: 'Calidad garantizada siempre'
-    }
-  ];
+  {
+    icono: 'water_drop',
+    titulo: 'FEATURES.ITEMS.ECO.TITLE',
+    descripcion: 'FEATURES.ITEMS.ECO.DESC'
+  },
+  {
+    icono: 'shield',
+    titulo: 'FEATURES.ITEMS.PROTECTION.TITLE',
+    descripcion: 'FEATURES.ITEMS.PROTECTION.DESC'
+  },
+  {
+    icono: 'star',
+    titulo: 'FEATURES.ITEMS.RATING.TITLE',
+    descripcion: 'FEATURES.ITEMS.RATING.DESC'
+  }
+];
 }
