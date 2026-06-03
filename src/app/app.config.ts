2cae337a07d7b7560f7cdd586a09e4582c78adfd
merari <merariurbano0322@gmail.com>
2026-06-03 14:36:22 -0500

    proof

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-
 import { provideHttpClient } from '@angular/common/http';
-
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
-//Importamos ionic
-import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-
     provideHttpClient(),
-
     provideTranslateService({
       defaultLanguage: 'es',
       loader: provideTranslateHttpLoader({
         prefix: './assets/i18n/',
         suffix: '.json'
       })
-    }),
-    //Ionic
-    provideIonicAngular()
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.html'
 })
 export class App {
