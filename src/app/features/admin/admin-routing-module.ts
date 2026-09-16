8970df7c1e3705994aea5cccb2c53a58faeae706
merari <merariurbano0322@gmail.com>
2026-09-16 14:02:28 -0500

    Revert "Merge branch 'feature/security' into develop"
    This reverts commit cdc9a51f63f24d55bb9484d2e061c296c4ff54f0, reversing
    changes made to e7c096358ca27de9fe1ea957764e0d135bb82e35.

--- src/app/features/admin/admin-routing-module.ts
+++ src/app/features/admin/admin-routing-module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard';
 import { ReportsComponent } from './pages/reports/reports';
 import { PaymentsComponent } from './pages/payments/payments';
-import { ManagementComponent } from './pages/management/management';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'reports', component: ReportsComponent },
   { path: 'payments', component: PaymentsComponent },
-  { path: 'management', component: ManagementComponent },
 ];
 
 @NgModule({
