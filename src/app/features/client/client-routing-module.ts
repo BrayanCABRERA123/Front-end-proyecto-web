239f1c0b4168ba43bafaecd0f6dd714a401bf45c
brayan <bspc1507@gmail.com>
2026-09-26 02:44:51 -0500

    fix(client): remove broken /client/ratings route

--- src/app/features/client/client-routing-module.ts
+++ src/app/features/client/client-routing-module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 // importamos las páginas
 import { DashboardComponent } from './pages/dashboard/dashboard';
 import { ProfileComponent } from './pages/profile/profile';
-import { RatingModalComponent } from '../../shared/dialogs/rating-modal/rating-modal';
 import { ReserveComponent } from './pages/reserve/reserve';
 import { ClientNotificationsComponent } from './pages/notifications/notifications';
 import { HistoryComponent } from './pages/history/history';
@@ -17,7 +16,6 @@ const routes: Routes = [
   // importamos las rutas
   { path: '', component: DashboardComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'ratings', component: RatingModalComponent},
   { path: 'reserve', component: ReserveComponent },
   { path: 'notifications', component: ClientNotificationsComponent},
   { path: 'history', component: HistoryComponent },
